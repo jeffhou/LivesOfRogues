74c633b9bfa81879827a97a31b11938b576d22cb
Jeff <j.king.hou@gmail.com>
2021-11-25 14:35:48 -0800

    Add target selection state

--- src/App.js
+++ src/App.js
@@ -7,8 +7,116 @@ import {
   useRecoilValue
 } from "recoil";
 
+const MoveSegment = {
+  newBuilder: function () {
+    return MoveSegment.Builder();
+  },
+  Builder: class {
+    setTarget(team, num) {
+      return this;
+    }
+    setEffect(type, amount) {
+      return this;
+    }
+  }
+};
+
+const moves = [
+  {
+    target: {
+      team: "ENEMY",
+      num: "1"
+    },
+    effect: {
+      type: "DAMAGE",
+      amount: "5"
+    }
+  }
+];
+
 const playerTeam = atom({
   key: "player-team",
+  default: [
+    {
+      name: "Justin the Great",
+      health: {
+        current: 20,
+        max: 24
+      },
+      moves: [
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        }
+      ]
+    },
+    {
+      name: "Brutus the Philosophical",
+      health: {
+        current: 15,
+        max: 24
+      },
+      moves: [
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        }
+      ]
+    },
+    {
+      name: "Luffy the Solemn",
+      health: {
+        current: 23,
+        max: 24
+      },
+      moves: [
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        },
+        {
+          name: "Basic Arrow",
+          description: "Shoot an opponent for 5 damage."
+        }
+      ]
+    }
+  ]
+});
+
+const enemyTeam = atom({
+  key: "enemy-team",
   default: [
     {
       name: "Eleanor the Great",
@@ -32,6 +140,17 @@ const playerTeam = atom({
       }
     }
   ]
+});
+
+const gamePhase = atom({
+  key: "game-phase",
+  default: {
+    phase: "target-select",
+    currentUnit: {
+      team: "player",
+      unitIndex: 0
+    }
+  }
 });
 
 function LifeBar(props) {
@@ -92,25 +211,27 @@ function CharacterIcon(props) {
 }
 
 function EnemyTeam(props) {
+  const [teamState, setTeamState] = useRecoilState(enemyTeam);
   return (
     <div className="player-team-container">
-      <CharacterIcon
-        data={{ name: "Eleanor the Great", currentHp: 12, maxHp: 20 }}
-      />
-      <CharacterIcon
-        data={{ name: "Eleanor the Great", currentHp: 12, maxHp: 20 }}
-      />
-      <CharacterIcon
-        data={{ name: "Eleanor the Great", currentHp: 12, maxHp: 20 }}
-      />
+      {teamState.map((unit) => (
+        <CharacterIcon
+          data={{
+            name: unit.name,
+            currentHp: unit.health.current,
+            maxHp: unit.health.max
+          }}
+        />
+      ))}
     </div>
   );
 }
+
 function PlayerTeam(props) {
-  const [team, setTeam] = useRecoilState(playerTeam);
+  const [teamState, setTeamState] = useRecoilState(playerTeam);
   return (
     <div className="player-team-container">
-      {team.map((unit) => (
+      {teamState.map((unit) => (
         <CharacterIcon
           data={{
             name: unit.name,
@@ -145,36 +266,87 @@ function MoveSelectionOption(props) {
   return (
     <div className="move-selection-option-container">
       <div className="move-selection-option-inner-container">
-        <div className="bold line_2">Ricochet Arrow</div>
-        <div>
-          Shoot an opponent for 5 damage and do 5 damage to a random enemy.
-        </div>
+        <div className="bold line_2">{props.move.name}</div>
+        <div>{props.move.description}</div>
       </div>
     </div>
   );
 }
 
 function MoveSelection(props) {
+  const [gamePhaseState, setGamePhaseState] = useRecoilState(gamePhase);
+  const [playerTeamState, setPlayerTeamState] = useRecoilState(playerTeam);
+  const [enemyTeamState, setEnemyTeamState] = useRecoilState(enemyTeam);
+
+  const moves =
+    gamePhaseState.currentUnit.team === "player"
+      ? playerTeamState[gamePhaseState.currentUnit.unitIndex].moves
+      : enemyTeamState[gamePhaseState.currentUnit.unitIndex].moves;
+
   return (
     <div className="move-selection-container">
       <div>
-        <MoveSelectionOption />
-        <MoveSelectionOption />
+        <MoveSelectionOption move={moves[0]} />
+        <MoveSelectionOption move={moves[1]} />
       </div>
       <div>
-        <MoveSelectionOption />
-        <MoveSelectionOption />
+        <MoveSelectionOption move={moves[2]} />
+        <MoveSelectionOption move={moves[3]} />
       </div>
+    </div>
+  );
+}
+function TargetSelection(props) {
+  const [teamState, setTeamState] = useRecoilState(playerTeam);
+  return (
+    <div className="target-selection-container">
+      {teamState.map((unit) => (
+        <div className="selection-button-container">
+          <div className="selection-button bounce-hover-container">
+            <div className="selection-button-inner-container">
+              <svg
+                className="animated bounce"
+                xmlns="http://www.w3.org/2000/svg"
+                width="48"
+                height="48"
+                viewBox="0 0 24 24"
+              >
+                <g color="#6c081a">
+                  <path
+                    d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"
+                    fill="currentColor"
+                  />
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
 function BattleScene(props) {
+  const [gamePhaseState, setGamePhaseState] = useRecoilState(gamePhase);
+
+  let gamePhaseContentDOM;
+  if (gamePhaseState.phase === "move-select") {
+    gamePhaseContentDOM = (
+      <MoveSelection currentUnit={{ name: "Peter the Sacred" }} />
+    );
+  } else if (gamePhaseState.phase === "move-details") {
+    gamePhaseContentDOM = (
+      <MoveDetails currentUnit={{ name: "Peter the Sacred" }} />
+    );
+  } else if (gamePhaseState.phase === "target-select") {
+    gamePhaseContentDOM = (
+      <TargetSelection currentUnit={{ name: "Peter the Sacred" }} />
+    );
+  }
   return (
     <div className="battle-scene">
       <EnemyTeam />
-      {/* <MoveSelection currentUnit={{ name: "Peter the Sacred" }} /> */}
-      <MoveDetails currentUnit={{ name: "Peter the Sacred" }} />
+      {gamePhaseContentDOM}
       <PlayerTeam />
     </div>
   );
